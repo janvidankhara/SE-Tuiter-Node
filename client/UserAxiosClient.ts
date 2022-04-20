7e9fbc893852eb342237f6dfaf56236b287e2490
Janvi Dankhara <janvidankhara@Janvis-MacBook-Pro.local>
2022-04-20 16:12:34 -0400

    make it better

--- client/UserAxiosClient.ts
+++ client/UserAxiosClient.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from 'axios';
 import User from "../models/User";
-axios.defaults.baseURL = 'http://localhost:4000/api';
-//axios.defaults.baseURL = 'https://cs5500assignment3.herokuapp.com/api';
+//axios.defaults.baseURL = 'http://localhost:4000/api';
+axios.defaults.baseURL = 'https://se-tuiter-project-backend.herokuapp.com/api';
 
 const findAllUsers = async () =>
     await axios.get('/users');
